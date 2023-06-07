5e8bbb82ae2856903fec58842a24fa99e9e6231f
shengpeng.ma <shengpeng.ma@nio.com>
2023-06-07 15:46:36 +0800

    增加上次更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,78 @@
+const getConfig = require("vuepress-bar");
+
+const { sidebar } = getConfig({
+  addReadMeToFirstGroup: false,
+  maxLevel: Number.MAX_VALUE,
+});
 module.exports = {
-    title: 'MSP 知识库',
-    description: 'MSP 知识库',
-    theme: 'reco',
-    base: '/blog/',
-    locales: {
-        '/': {
-            lang: 'zh-CN'
-        }
+  head: [
+    [
+      "link", // 设置 favicon.ico，注意图片放在 public 文件夹下
+      { rel: "icon", href: "icon.png" },
+    ],
+  ],
+  title: "MSP 知识库",
+  description: "MSP 知识库",
+  theme: "reco",
+  base: "/blog/",
+  locales: {
+    "/": {
+      lang: "zh-CN",
     },
-    themeConfig: {
-        subSidebar: 'auto',
-        nav: [
-            { text: '首页', link: '/' },
-            {
-                text: 'MSP',
-                items: [
-                    { text: 'Github', link: 'https://github.com/mashengpeng' },
-                ]
-            }
-        ],
-        sidebar: [
-            {
-                title: '欢迎学习',
-                path: '/',
-                collapsable: false, // 不折叠
-                children: [
-                    { title: "学前必读", path: "/" }
-                ]
-            },
-            {
-                title: '欢迎学习',
-                path: '/handbook/third',
-            },
-            {
-                title: "基础学习",
-                path: '/handbook/first',
-                collapsable: true, // 不折叠
-                children: [
-                    { title: "first", path: "/handbook/first" },
-                    { title: "second", path: "/handbook/second" }
-                ],
-            }
-        ]
-    }
-}
+  },
+  themeConfig: {
+    lastUpdated: "上次更新", // string | boolean
+    subSidebar: "auto",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "MSP",
+        items: [{ text: "Github", link: "https://github.com/mashengpeng" }],
+      },
+    ],
+    sidebar,
+  },
+  plugins: [
+    [
+      "@vuepress/last-updated",
+      {
+        transformer: (timestamp, lang) => {
+          // 不要忘了安装 moment
+          const moment = require("moment");
+          moment.locale(lang);
+          return moment(timestamp).fromNow();
+        },
+      },
+    ],
+    [
+      "vuepress-plugin-code-copy",
+      {
+        selector: 'div[class*="language-"] pre',
+        align: "bottom",
+        color: "#59b870",
+        backgroundTransition: false,
+        backgroundColor: "#0075b8",
+        successText: "Copied!",
+      },
+    ],
+    [
+      "cursor-effects",
+      {
+        size: 2, // size of the particle, default: 2
+        shape: "star", // ['star' | 'circle'], // shape of the particle, default: 'star'
+        zIndex: 999999999, // z-index property of the canvas, default: 999999999
+      },
+    ],
+    [
+      "dynamic-title",
+      {
+        // showIcon: "../public/icon.png",
+        showText: "客官欢迎回来~",
+        // hideIcon:
+        //   "https://www.typescriptlang.org/favicon-32x32.png?v=8944a05a8b601855de116c8a56d3b3ae",
+        hideText: "客官不要走嘛~",
+        recoverTime: 2000,
+      },
+    ],
+  ],
+};
